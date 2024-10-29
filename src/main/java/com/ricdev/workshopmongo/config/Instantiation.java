4c459b879f93ada5ba81011a5a39e190326ddf59
Ricardo-Alves-Dev <rpsevla@gmail.com>
2024-10-29 13:27:21 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/ricdev/workshopmongo/config/Instantiation.java
@@ -0,0 +1,26 @@
+	package com.ricdev.workshopmongo.config;
+
+	import java.util.Arrays;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.boot.CommandLineRunner;
+	import org.springframework.context.annotation.Configuration;
+	import com.ricdev.workshopmongo.domain.User;
+	import com.ricdev.workshopmongo.repository.UserRepository;
+	
+	@Configuration
+	public class Instantiation implements CommandLineRunner {
+		
+		@Autowired
+		private UserRepository userReposiroty;
+		
+		@Override
+		public void run(String... arg0) throws Exception {
+			
+						
+			User maria = new User(null, "Maria Brown", "maria@gmail.com");
+			User alex = new User(null, "Alex Green", "alex@gmail.com");
+			User bob = new User(null, "Bob Grey", "bob@gmail.com");
+			
+			userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		}
+	}
